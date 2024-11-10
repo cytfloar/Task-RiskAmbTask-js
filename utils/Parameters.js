e288c6dd80574c68f655b1ecf1dc67df8c33a05d
Serena Gu <cytfloar@gmail.com>
2024-11-10 00:18:46 -0500

    forgot to push oops!

--- utils/Parameters.js
+++ utils/Parameters.js
@@ -1,3 +1,13 @@
+function randperm(n) {
+    let permutation = Array
+        .from({ length: n }, (_, i) => i + 1);
+
+    permutation
+        .sort(() => Math.random() - 0.5);
+
+    return permutation;
+}
+
 // paramPrep = [vals probs ambigs]
 
 /////This part computed [vals probs ambigs], the matrix is converted and saved as paramPrep.json
@@ -10,3 +20,6 @@
 // ambigLevels=[0.24,0.5,0.74];
 // [valsA, ambigs, repeat]=ndgrid(ambigVals,ambigLevels, 1:n_repeats);
 
+colorIndex1=[1, 2, 5, 6, 7, 11, 14, 15, 18, 20];
+colorIndex2=[3, 4, 8, 9, 10, 12, 13, 16, 17, 19];
+//paramPrep=[vals probs ambigs shufColors' randperm(length(vals))'];
